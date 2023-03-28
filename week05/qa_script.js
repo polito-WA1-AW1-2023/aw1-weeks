97fa5a6bb6822947088974d89f97f46d58297cb5
Enrico Masala <enrico.masala@polito.it>
2023-03-28 10:32:58 +0200

    Ora che cambia

--- week05/qa_script.js
+++ week05/qa_script.js
@@ -1,4 +1,14 @@
 "use strict";
 
-document.getElementsByTagName("table")[0].classList.remove("table");
+let p = document.createElement('p');
+let d = dayjs().format("YYYY-MM-DD HH:mm:ss");
+p.innerText = d;
+document.getElementById("time").appendChild(p);
+
+setInterval(  ()=> p.innerText = dayjs().format("YYYY-MM-DD HH:mm:ss") , 1000);
+
+
+
+
+//document.getElementsByTagName("table")[0].classList.remove("table");
 
