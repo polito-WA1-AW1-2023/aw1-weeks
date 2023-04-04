bf9a96a42c6e8b68b4ab7e45a05a3fe20638c4fd
Enrico Masala <enrico.masala@polito.it>
2023-04-04 10:07:22 +0200

    Esempio in aula: my-app

---
+++ week06/my-app/src/Button.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+function MyButton(props) {
+    let [buttonLang, setButtonLang] = useState(props.lang);
+    if (buttonLang === 'it')
+        return <Button variant='secondary' onClick={() => setButtonLang('en')}>Ciao!</Button>
+    else
+        return <Button variant='primary' onClick={() => setButtonLang('it')}>Hello!</Button>
+}
+export default MyButton;
